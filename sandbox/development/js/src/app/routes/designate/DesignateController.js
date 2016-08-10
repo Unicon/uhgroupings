8664c4d935713055a8c5a060c5ef2f29b08921a7
Chris Paraiso <cparaiso@unicon.net>
2016-08-10 13:45:18 -0700

    [SUG-5] modified Desginate Admins view for admin data, wired in remove admin functionality, transformed remove admin call to send json obj instead of string.

--- sandbox/development/js/src/app/routes/designate/DesignateController.js
+++ sandbox/development/js/src/app/routes/designate/DesignateController.js
@@ -34,12 +34,52 @@ angular.module('routes.designate.DesignateController', [
         designateCtrl = this;
 
         /**
+         * Property houses flags indicating current state of the UI.
+         *
+         * @property uiState
+         * @type {Object}
+         */
+        designateCtrl.uiState = {
+            isLoadingAdmins: true
+        };
+
+        /**
          * Property houses a reference to authenticated user object.
          *
          * @property designateCtrl.user
          * @type {Object}
          */
         designateCtrl.user = protect;
+
+        /**
+         * Method to handle removing an admin.
+         *
+         * @method removeAdmin
+         * @param string userId
+         */
+        designateCtrl.removeAdmin = function (userId) {
+            console.log('removeAdmin', userId);
+            designateCtrl.admins = _.reject(designateCtrl.admins, function (admin) {
+                return userId == admin.userId;
+            });
+            AdminsService.removeAdmin(userId).then(function (response) {
+                console.log(response);
+            });
+        };
+
+        /**
+         * Method to handle removing an admin. This handler
+         * is executed when the enter key is detected.
+         *
+         * @method removeAdmin
+         * @param {Object} Event Event object
+         * @param string userId
+         */
+        designateCtrl.removeAdminOnKeyDown = function (e, userId) {
+            if (e.keyCode === 13) {
+                designateCtrl.removeAdmin(userId);
+            }
+        };
 
         /**
          * Method executes initialization process.
@@ -50,7 +90,9 @@ angular.module('routes.designate.DesignateController', [
         function initialize() {
             var t = $timeout(function () {
                 AdminsService.list().then(function (admins) {
+                    console.log(admins)
                     designateCtrl.admins = admins.data;
+                    designateCtrl.uiState.isLoadingAdmins = false;
                 });
                 // Call implementations here. Timeout is needed in order
                 // for all potentially nested directives to execute.
