47a07cc5e4cf70042eb63c6b09e3d17e0ce0c21c
Chris Paraiso <cparaiso@unicon.net>
2016-08-19 16:08:44 -0700

    [SUG-9] Groupings Search Screen, default members pagination

--- sandbox/development/js/src/app/routes/grouping-search/grouping-editor/membership/default/DefaultGroupContentController.js
+++ sandbox/development/js/src/app/routes/grouping-search/grouping-editor/membership/default/DefaultGroupContentController.js
@@ -1,5 +1,6 @@
 angular.module('routes.groupingSearch.DefaultGroupContentController', [
-    'stack.i18n'
+    'stack.i18n',
+    'stack.pagination.UserPagination'
 ])
 
 /**
@@ -11,8 +12,10 @@ angular.module('routes.groupingSearch.DefaultGroupContentController', [
  * @module routes.groupingSearch.DefaultGroupContentController
  */
 .controller('DefaultGroupContentController', [
+    '$timeout',
     '$scope',
-    function ($scope) {
+    'USER_PAGINATION',
+    function ($timeout, $scope, USER_PAGINATION) {
         // Define.
         var defaultGroupContentController;
 
@@ -39,6 +42,37 @@ angular.module('routes.groupingSearch.DefaultGroupContentController', [
          * @type {Object}
          */
         defaultGroupContentController.grouping = $scope.groupingEditorCtrl.grouping;
+
+        /**
+         * Property houses an object for pagination properties.
+         *
+         * @property defaultGroupContentController.pagination
+         * @type {Object}
+         */
+        defaultGroupContentController.pagination = {};
+
+        /**
+         * Method slicing defaultMembers array into pages.
+         *
+         * @method sliceForPagination
+         * @private
+         */
+        function sliceForPagination() {
+            var offset = (defaultGroupContentController.pagination.currentPage * defaultGroupContentController.pagination.itemsPerPage) - defaultGroupContentController.pagination.itemsPerPage;
+            defaultGroupContentController.grouping.defaultMembersPaginated = defaultGroupContentController.grouping.defaultMembers.slice(
+                offset, offset + defaultGroupContentController.pagination.itemsPerPage
+            );
+        }
+
+        /**
+         * Method to handle changing pages.
+         *
+         * @method pageChanged
+         */
+        defaultGroupContentController.pageChanged = function () {
+            console.log('Page changed to: ' + defaultGroupContentController.pagination.currentPage);
+            sliceForPagination();
+        };
 
         /**
          * Method to easily control adjusting the sort of the owners table.
@@ -68,18 +102,28 @@ angular.module('routes.groupingSearch.DefaultGroupContentController', [
             // of each group.
             //
             // TODO: move this code into a service.
-            defaultGroupContentController.grouping.defaultMembers = defaultGroupContentController.grouping.basisMembers.map(function (m) {
-                    m.sourceGroup = 'Basis';
-                    return m;
-                })
-                .concat(defaultGroupContentController.grouping.includedMembers.map(function (m) {
-                    m.sourceGroup = 'Include';
-                    return m;
-                }))
-                .filter(function (m) {
-                    return defaultGroupContentController.grouping.excludedMemberIds.indexOf(m.userId) === -1;
-                }
-            );
+            var t = $timeout(function () {
+                defaultGroupContentController.grouping.defaultMembers = defaultGroupContentController.grouping.basisMembers.map(function (m) {
+                        m.sourceGroup = 'Basis';
+                        return m;
+                    })
+                    .concat(defaultGroupContentController.grouping.includedMembers.map(function (m) {
+                        m.sourceGroup = 'Include';
+                        return m;
+                    }))
+                    .filter(function (m) {
+                        return defaultGroupContentController.grouping.excludedMemberIds.indexOf(m.userId) === -1;
+                    }
+                );
+
+                defaultGroupContentController.pagination.totalItems = defaultGroupContentController.grouping.defaultMembers.length;
+                defaultGroupContentController.pagination.itemsPerPage = USER_PAGINATION.PAGE_SIZE;
+                defaultGroupContentController.pagination.currentPage = USER_PAGINATION.PAGE_NUMBER;
+                sliceForPagination();
+                // Call implementations here. Timeout is needed in order
+                // for all potentially nested directives to execute.
+                $timeout.cancel(t);
+            }, 0);
         }
         initialize();
     }
